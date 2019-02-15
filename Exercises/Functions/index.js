8491751832d14ed418f1f264b2474e16460dfcc7
Marie-Louise Annan <m.annan@wellcome.ac.uk>
2019-02-15 09:04:24 +0000

    add more functions

--- Exercises/Functions/index.js
+++ Exercises/Functions/index.js
@@ -60,8 +60,19 @@ console.log(whatDoYouDo('driver', 'sally'));
 console.log(whatDoYouDo('designer', 'clare'));
 console.log(whatDoYouDo('architect', 'anthony'));
 
-//javascript expressions are pieces of code that always produce a value
-//javascript declarations don't produce any immediate value so therefore are called statements
+//javascript expressions are pieces of code that always produce a value - (according to udemy course but I need to check this in the o'reilly js handbook)
+//javascript declarations don't produce any immediate value so therefore are called statements - (according to udemy course but I need to check this in the o'reilly js handbook)
+
+//function expressions - In a function expression the name of the function isn't used and is instead passed to a variable. A function without a name is called an anonymous function (codecademy)
+
+const plantNeedsWater = function(day){
+  if(day ==='Wednesday'){
+    return true;
+  } else { return false;
+  }
+}
+
+console.log(plantNeedsWater('Tuesday'));
 
 
 //arrow functions
@@ -82,3 +93,51 @@ console.log(divide(12, 3));
 var divide2 = ( x , y) => x / y;
 
 console.log(divide2(12,3));
+
+//ES6 default parameters
+
+function makeShoppingList(item1='milk',item2='bread',item3='eggs'){
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
+makeShoppingList();
+
+//ES6 Arrow functions - arrow function syntax is a shorter way to write functions "fat arrow" ()=> notation (codecademy)
+
+const plantNeedsWater2 = (day) => {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// concise body arrow functions - refactoring arrow functions
+
+//zero parameters
+
+//functions with one parameter don't need to be enclosed in brackets
+const functionName = paramOne => {};
+
+
+// using the return keyword to calculate the number of monitors needed in an office
+
+function monitorCount(rows, columns){
+  return rows * columns;
+}
+const numOfMonitors = monitorCount(5,4);
+console.log(numOfMonitors);
+
+//helper functions - are functions being called within another function
+
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+function costOfMonitors(rows, columns) {
+  return monitorCount(rows,columns)*200
+};
+
+const totalCost = costOfMonitors(5,4);
+
+console.log(totalCost);
